c0fc7431181aa47b8b84b0c43adefdbd637946e1
gurubasva <112559275+gurubasva@users.noreply.github.com>
2022-09-14 19:30:42 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -13,7 +13,7 @@ void factorial()
         for (i = 1; i <= n; ++i) {
             fact *= i;
         }
-        printf("Factorial of %d = %llu\n", n, fact);
+        printf("Factorial of the nunber %d = %llu\n", n, fact);
     }
 
     //return 0;
